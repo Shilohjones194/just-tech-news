d8ed1dd84de739ec672f448aed47263cf362a60b
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-06-05 23:09:58 -0700

    13.3.6 test

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 // this file will become more important as we create more models, but for now itll just be for collection and exporting the User model data.
 // 13.1.5//
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+// statement above we are defining, the relation ship of Post model to the User
+
+module.exports = { User, Post };
