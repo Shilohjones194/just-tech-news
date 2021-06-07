35577c881595090e2b47c0b10af7639665752e7a
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-06-07 07:59:59 -0700

    13.4.3 Vote route completed

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 // 13.1.5//
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -11,5 +12,32 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 // statement above we are defining, the relation ship of Post model to the User
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote };
